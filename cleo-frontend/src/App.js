1dd628eb27d8da65ca3596467ded5341016348b6
Ayush Gaykar <166710636+Shrol69@users.noreply.github.com>
2025-02-22 17:39:45 +0530

    done with caht

--- cleo-frontend/src/App.js
+++ cleo-frontend/src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3000"); // Replace with backend URL when deployed
+const socket = io("http://localhost:5000"); // Replace with backend URL when deployed
 
 function App() {
   const [message, setMessage] = useState("");
